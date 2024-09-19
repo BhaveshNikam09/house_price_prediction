ac2201416cff55499ee40e8eabd122d9b31a9aec
omii1809 <137719427+omii1809@users.noreply.github.com>
2024-09-19 11:49:11 +0530

    created full template

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+    name="HousePricePrediction",
+    version="0.0.1",
+    author="Bhavesh Nikam",
+    author_email="nikambhavesh752@gmail.com",
+    packages=find_packages()
+    
+)
